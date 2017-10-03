0c338e16dbd693418c99f0c964aa71953f468873
AiGon <32458798+AiGon@users.noreply.github.com>
2017-10-03 20:16:56 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-const Telegraf = require('telegraf')
+const TelegramBot = require('node-telegram-bot-api');
 
-const app = new Telegraf('402660687:AAE783zHNDSyXg8RM_SvQxxufVk648zvkOk')
-app.command('start', ({ from, reply }) => {
-  console.log('start', from)
-  return reply('Welcome!')
-})
-app.hears('hi', (ctx) => ctx.reply('Hey there!'))
-app.on('sticker', (ctx) => ctx.reply('👍'))
-app.startPolling()
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '402660687:AAE783zHNDSyXg8RM_SvQxxufVk648zvkOk';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
 
 //402660687:AAE783zHNDSyXg8RM_SvQxxufVk648zvkOk
 
